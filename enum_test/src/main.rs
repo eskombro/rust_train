81f646de8e5c5f93d550f0bfc0a21138a9a16cc4
Samuel Jimenez <sjimenezre@gmail.com>
2020-02-05 11:05:44 +0100

    Implement enum

--- enum_test/src/main.rs
+++ enum_test/src/main.rs
@@ -4,25 +4,28 @@ enum Animal {
     Cat,
 }
 
-fn do_noise(animal: &Animal) -> String {
-    match animal {
-        Animal::Horse => String::from("A HORSE says BAHAHA"),
-        Animal::Dog => String::from("A DOG says BARF"),
-        Animal::Cat => String::from("A CAT says MIAOU"),
-    }
-}
+impl Animal {
 
-fn eat(animal: &Animal) -> String {
-    match animal {
-        Animal::Horse => String::from("and eats CARROTS"),
-        Animal::Dog => String::from("and eats ANYTHING"),
-        Animal::Cat => String::from("and eats FISH"),
+    fn do_noise(&self) -> String {
+        match self {
+            Animal::Horse => String::from("A HORSE says BAHAHA"),
+            Animal::Dog => String::from("A DOG says BARF"),
+            Animal::Cat => String::from("A CAT says MIAOU"),
+        }
+    }
+
+    fn eat(&self) -> String {
+        match self {
+            Animal::Horse => String::from("and eats CARROTS"),
+            Animal::Dog => String::from("and eats ANYTHING"),
+            Animal::Cat => String::from("and eats FISH"),
+        }
     }
 }
 
 fn main() {
     let zoo = vec![Animal::Horse, Animal::Dog, Animal::Cat];
     for animal in zoo {
-        println!("{} {}", do_noise(&animal), eat(&animal));
+        println!("{} {}", animal.do_noise(), animal.eat());
     }
 }
